f658f363343be1f3561082eff261c70038907eb9
Hyde <seokjinyoo0716@gmail.com>
2024-04-05 13:06:56 -0400

    final fix to the ip ban

--- src/routes/finalcode/+page.server.ts
+++ src/routes/finalcode/+page.server.ts
@@ -2,31 +2,28 @@ import { db } from '$lib/store';
 import { redirect } from '@sveltejs/kit';
 import { collection, getDocs } from 'firebase/firestore';
 
+export const load = async (event) => {
+  const ips: string[] = [];
 
-const ips: string[] = [];
-
-const querySnapshot = await getDocs(collection(db, 'ips'));
-querySnapshot.forEach((doc) => {
+  const querySnapshot = await getDocs(collection(db, 'ips'));
+  querySnapshot.forEach((doc) => {
     ips.push(doc.data().ip);
     // console.log(`${doc.id} => ${doc.data()}`, doc.data());
-});
+  });
 
-// console.log(ips);
-
-export const load = async (event) => {
-    let requestIp: string = "";
-    try {
-      requestIp = event.getClientAddress(); // IP from Client Request
-      if (!requestIp) {
-        throw new Error("requestIp is null");
-      }
-      if (ips.includes(requestIp)) {
-        console.log("You are not allowed to access this page.")
-        redirect(302, '/blank')
-      }
-    } catch (error) {
-      console.log('Error: ', error);
+  let requestIp: string = "";
+  try {
+    requestIp = event.getClientAddress(); // IP from Client Request
+    if (!requestIp) {
+      throw new Error("requestIp is null");
     }
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+  if (ips.includes(requestIp)) {
+    console.log("You are not allowed to access this page.")
+    redirect(302, '/blank')
+  }
 
-    return { ip: requestIp, bannedIps: ips }
+  return { ip: requestIp, bannedIps: ips }
 }
