21f1425ba78a4d0e9e2a4f6e0c49210275b48cfa
Hyde <seokjinyoo0716@gmail.com>
2024-04-05 10:48:20 -0400

    removed support for old browsers for top-level-await

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	esbuild: {
+		supported: {
+			'top-level-await': true,
+
+		},
+	},
 	plugins: [sveltekit()]
 });
