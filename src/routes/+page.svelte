16d2fe674ce2a49f8ada4ce60d75f4e11e5e4dda
hydecnc <seokjinyoo0716@gmail.com>
2024-04-22 09:45:27 -0600

    added contacts, highlights & code formatted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,10 @@
 
 	<h2 class="glitch-text" id="glitch-by">Presented by THOTH</h2>
 	<h1 class="glitch-text">The GLITCH</h1>
-	<button class="glitch-text glitch-date {animationRunning ? '' : 'paused'}" on:click={toggleBinaryDate}>{dateString}</button>
+	<button
+		class="glitch-text glitch-date {animationRunning ? '' : 'paused'}"
+		on:click={toggleBinaryDate}>{dateString}</button
+	>
 </div>
 
 <div id="content">
@@ -40,36 +43,49 @@
 	<div class="desc-text" id="main-desc">
 		The THOTH database has been hacked by an unknown hacker group. They have put the THOTH database,
 		which contains all problems the THOTH club has ever solved, at a risk of deletion. They must be
-		stopped at all cost. Whoever solves the situtation most deligently will be awarded 
+		stopped at all cost. Whoever solves the situation most deligently will be awarded
 		<strong>$3000 MXN</strong>
-		by the THOTH club. You have been called to save the database from the hackers and save the
-		THOTH database.
+		by the THOTH club. You have been called to save the database from the hackers and save the THOTH
+		database.
 		<br />
 		<br />
-		To save the THOTH database, you must solve each problems given in the stations and also find the
+		To save the THOTH database, you must solve each problem given in the stations and also find the
 		<strong>Final Code</strong> required to stop the glitchers.
 	</div>
 	<div class="desc-text">
 		<h2 id="station-title">Instructions</h2>
 		<ul>
-			<li>Only ONE registration team must be submitted per team.</li>
-			<li>You can participate as an individual or in a team of maximum 3 people.</li>
+			<li><strong>Only ONE</strong> registration form must be submitted per team.</li>
 			<li>
-				Only ASFM high school students can participate, but you may have different grade levels in
-				one team.
+				You can participate as <strong>an individual</strong> or in
+				<stronog>a team of maximum 3 people</stronog>.
 			</li>
-			<li>The registration will close on May 10th 11:59 p.m.</li>
+			<li>
+				Only <strong>ASFM high school students</strong> can participate, but you may have different grade
+				levels in one team.
+			</li>
+			<li>The registration will <strong>close on May 10th</strong> 11:59 p.m.</li>
 		</ul>
 		<div class="block-desc">
 			* No advanced mathematics will be required for the event. Any high school student can have
-			success in the event. 
+			success in the event.
+		</div>
+		<div class="block-desc">
+			For any inquiries, please contact:
+			<a
+				class="contact"
+				href="https://www.instagram.com/th0th_asfm?igsh=eW55MWh6NjU5aHJl"
+				target="_blank"
+			>
+				<strong> @th0th_asfm </strong>
+			</a>
+			or <a class="contact" href="mailto:24yoo9560@asfm.mx"><strong>24yoo9560@asfm.mx</strong></a>
 		</div>
 	</div>
 </div>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
-
 
 	h1 {
 		font-size: 6rem;
@@ -122,7 +138,7 @@
 		background-size: 100% 7px;
 		text-align: center;
 	}
-	
+
 	.glitch-filter {
 		height: 100%;
 		width: 100%;
@@ -131,30 +147,30 @@
 		top: 0%;
 		pointer-events: none;
 	}
-	
+
 	.glitch-date {
 		font-size: 1.4em;
-		animation: blink 1.0s linear infinite;
+		animation: blink 1s linear infinite;
 	}
-	
+
 	@keyframes blink {
 		0% {
-			opacity: 1.0;
+			opacity: 1;
 		}
 		50% {
 			opacity: 0.5;
 		}
 		100% {
-			opacity: 1.0;
+			opacity: 1;
 		}
 	}
-	
+
 	.paused {
 		animation: none;
 		/* animation-play-state: paused; */
-		opacity: 1.0;
+		opacity: 1;
 	}
-	
+
 	#main-desc {
 		font-size: 1.2em;
 	}
@@ -166,19 +182,19 @@
 			rgb(var(--darker-rgb) / 15%) 56%,
 			rgb(var(--darker-rgb) / 5%) 56%,
 			rgb(var(--darker-rgb) / 5%) 100%
-			);
-			background-size: 100% 7px;
-			z-index: 5;
-			opacity: 0.75;
-			animation: pan-lines 360s infinite linear;
-		}
-		
-		#content {
-			display: flex;
-			align-items: center;
-			flex-direction: column;
-			gap: 1em;
-			animation: content-fade-in 3.0s ease forwards;
+		);
+		background-size: 100% 7px;
+		z-index: 5;
+		opacity: 0.75;
+		animation: pan-lines 360s infinite linear;
+	}
+
+	#content {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		gap: 1em;
+		animation: content-fade-in 3s ease forwards;
 	}
 
 	@keyframes content-fade-in {
@@ -189,7 +205,7 @@
 			transform: translateX(0%);
 		}
 	}
-	
+
 	.block-desc {
 		flex: 1;
 		font-size: 0.9rem;
@@ -197,7 +213,7 @@
 		padding-right: 10px;
 		margin-bottom: 1em;
 	}
-	
+
 	.desc-text {
 		padding: 1rem;
 		/* font-family: 'Courier New', Courier, monospace; */
@@ -226,5 +242,9 @@
 		border-radius: 20px;
 		margin-bottom: 1.5em;
 		/* background-color: rgb(var(--darker-rgb)); */
+	}
+
+	.contact {
+		text-decoration: none;
 	}
 </style>
